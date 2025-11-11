1b6d0a665d6107c3397bf04468af9b5d2ab497f3
James Douglass <jdouglass@stanford.edu>
2025-11-11 15:47:27 -0800

    Working on tests.

--- tests.py
+++ tests.py
@@ -1,8 +1,20 @@
+import os
 import shutil
+import subprocess
 import tempfile
 import unittest
 
 from invest_ucm_calibration import plugin
+
+CWD = os.path.dirname(__file__)
+DATA = os.path.join(CWD, 'data')
+DATA_ZIP = 'https://github.com/martibosch/invest-ucm-calibration/archive/refs/tags/v0.6.0.zip'
+
+if not os.path.exists(DATA):
+    print(f"Downloading data directory from {DATA_ZIP}")
+    subprocess.run(['wget', DATA_ZIP, -O, 'data.zip'])
+    subprocess.run(['unzip', 'tests/data', '-d', DATA])
+    os.remove('data.zip')
 
 
 class UCMCalibrationPluginTests(unittest.TestCase):
@@ -13,4 +25,19 @@ class UCMCalibrationPluginTests(unittest.TestCase):
         shutil.rmtree(self.workspace)
 
     def test_execute(self):
-        plugin.execute({})
+        args = {
+            'workspace_dir': self.workspace_dir,
+            'lulc_raster_path': 'foo',
+            'cc_method': 'intensity',
+            'aoi_vector_filepath': '',
+        }
+        plugin.execute(args)
+
+    def test_validate(self):
+        args = {
+            'workspace_dir': self.workspace_dir,
+            'lulc_raster_path': '',
+            'cc_method': 'intensity',
+            'aoi_vector_path': ''
+        }
+        self.assertEqual([], plugin.validate(args))
